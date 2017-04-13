ff8a91532e864f722af800cc03fdbcbe0d7160c0
qc449 <qc449@nyu.edu>
2017-04-12 22:40:39 -0400

    asg10-1-1

---
+++ mpi_assignment_1.py
@@ -0,0 +1,17 @@
+''' 
+Author: Qiming Chen
+Date: Apr 12 2017
+Description: print “Hello” with even rank and print “Goodbye” with odd rank.
+Call by: mpiexec -n <the total number of processes> python mpi_assignment_1.py 
+'''
+
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD
+size = comm.Get_size() # get the total number of all processes
+rank = comm.Get_rank() # index of a particular process
+
+if rank % 2 == 0: # even
+    print("Hello from process", rank)
+else: # odd
+	print("Goodbye from process", rank)
